3854debaea686b089cb02b3188543451b242bdab
rnehacodes <neharajputcoding@gmail.com>
2024-04-07 23:01:58 +0530

    Virtual & Override Keywords

---
+++ VirtualOverrideDemo/Dog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOverrideDemo
+{
+    internal class Dog : Animal
+    {
+        bool IsHappy { get; set; }
+        public Dog(string name, int age) : base(name, age)
+        {
+            IsHappy = true;
+        }
+        public override void Eat()
+        {
+            base.Eat();
+        }
+        public override void MakeSound()
+        {
+            Console.WriteLine("Woof-Woof");
+        }
+    }
+}
---
+++ VirtualOverrideDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace VirtualOverrideDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog mydog = new Dog("Goofy", 3);
+            Console.WriteLine($"{mydog.Name} is {mydog.Age} years old!");
+            mydog.Eat();
+            mydog.Play();
+            mydog.MakeSound();
+        }
+    }
+}
