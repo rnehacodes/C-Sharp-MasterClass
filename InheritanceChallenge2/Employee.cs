6fd24ef23eac9b9a5e25937486b0f04f0bc936ae
rnehacodes <neharajputcoding@gmail.com>
2024-04-10 23:57:29 +0530

    Inheritance Challenge

---
+++ InheritanceChallenge2/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceChallenge2
+{
+    internal class Employee
+    {
+        public string Name {  get; set; }
+        public string FirstName {  get; set; }
+        public int Salary {  get; set; }
+
+        //Constructor
+        public Employee(string name, string firstName, int salary)
+        {
+            this.Name = name;
+            this.FirstName = FirstName;
+            this.Salary = salary;
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("{0} has started working.", Name);
+        }
+        public void Pause()
+        {
+            Console.WriteLine("{0}'s work has been paused.", Name);
+        }
+    }
+}
