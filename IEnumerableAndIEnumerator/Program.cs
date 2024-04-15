5a93d33a63e73b6bed4cfdfbe4b119db704f2342
rnehacodes <neharajputcoding@gmail.com>
2024-04-15 22:53:06 +0530

    IEnumerableAndIEnumerator

---
+++ IEnumerableAndIEnumerator/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace IEnumerableAndIEnumerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DogShelter shelter = new DogShelter();
+
+            foreach(Dog dog in shelter)
+            {
+                //
+            }
+        }
+    }
+
+    class Dog
+    {
+        public string Name { get; set; }
+        public bool IsNaughty { get; set; }
+        public Dog(string name, bool isNaughty)
+        {
+            Name = name;
+            IsNaughty = isNaughty;
+        }
+        public void GiveTreat(int numberOfTreats)
+        {
+            Console.WriteLine("Dog: {0} said wuoff {1} times!.", Name, numberOfTreats);
+        }
+    }
+    class DogShelter
+    {
+        public List<Dog> dogs;
+        public DogShelter()
+        {
+            dogs = new List<Dog>()
+            {
+                new Dog("Casper", false),
+                new Dog("Olie", true),
+                new Dog("Vinni", true),
+                new Dog("Goofy", false)
+            };
+        }
+    }
+}
