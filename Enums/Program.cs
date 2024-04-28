2e7bd771a39aa2dbb7594ed6b7e789ce520599de
rnehacodes <neharajputcoding@gmail.com>
2024-04-28 23:13:50 +0530

    File I/O assingment, Structs & Enums

---
+++ Enums/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Enums
+{
+    enum Day { Mon, Tue, Wed, Thu, fri, Sat, Sun};
+    enum Month { Jan = 2401, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec};
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Day today = Day.Sun;
+            Day test = Day.Mon;
+            Day test2 = Day.Sun;
+
+            Console.WriteLine(today == test);
+            Console.WriteLine(today == test2);
+            Console.WriteLine(today);
+            Console.WriteLine((int)test);
+
+            Console.WriteLine((int)Month.Dec);
+        }
+    }
+}
