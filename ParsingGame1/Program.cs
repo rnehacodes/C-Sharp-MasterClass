80e7bb77aefe2bf83800185caca770da87f7da9b
rnehacodes <neharajputcoding@gmail.com>
2024-04-28 23:15:23 +0530

    File I/O assingment, Structs & Enums

---
+++ ParsingGame1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.SqlTypes;
+
+namespace ParsingGame1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fileLines = System.IO.File.ReadAllLines(@"C:\Users\neha.rajput\Desktop\Learning\C#MasterClass\ParsingGame1\input.txt");
+            string outputText = "";
+            foreach(string line in fileLines)
+            {
+                if (line.Contains("split"))
+                {
+                    string[] splitLine = line.Split(" ");
+                    string enterText = splitLine[4];
+                    if(outputText == "")
+                    {
+                        outputText = enterText;
+                    }
+                    else
+                    {
+                        outputText = outputText + " " + enterText;
+                    }
+                }
+            }
+            File.WriteAllText(@"C:\Users\neha.rajput\Desktop\Learning\C#MasterClass\ParsingGame1\FileOutput\" + "output" + ".txt", outputText);
+        }
+    }
+}
---
+++ Structs/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Structs
+{
+    internal class Program
+    {
+        struct Game
+        {
+            public string Name;
+            public string Description;
+            public Game(string name, string description)
+            {
+                Name = name;
+                Description = description;
+            }
+            public override string ToString()
+            {
+                return $"Game Name: {Name}\nGame Description: {Description}";
+            }
+        }
+        static void Main(string[] args)
+        {
+            Game myGame = new Game();
+            myGame.Name = "Test";
+            myGame.Description = "Test game's test description";
+
+            Console.WriteLine(myGame);
+        }
+    }
+}
