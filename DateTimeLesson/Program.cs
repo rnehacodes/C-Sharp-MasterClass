c26ac020cf663e986471ec4cc5489bd725949203
rnehacodes <neharajputcoding@gmail.com>
2024-05-15 23:58:32 +0530

    DateTime

---
+++ DateTimeLesson/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace DateTimeLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = new DateTime(2023, 12, 01);
+            Console.WriteLine($"Date: {dateTime}\n");
+
+            DateTime dateTimeToday = DateTime.Today;
+            Console.WriteLine($"Date: {dateTimeToday}\n");
+
+            DateTime dateTimeTomorrow = GetTomorrow();
+            Console.WriteLine($"Date: {dateTimeTomorrow}\n");
+
+            DateTime firstdayOfYear = GetFirstDayoftheYear(2025);
+            Console.WriteLine($"Date: {dateTimeTomorrow}\n");
+
+            DateTime currentDateTime = DateTime.Now;
+            Console.WriteLine($"The date & time right now is {currentDateTime}\n");
+        }
+
+        private static DateTime GetTomorrow()
+        {
+            return DateTime.Today.AddDays(1);
+        }
+
+        private static DateTime GetFirstDayoftheYear(int year)
+        {
+            return new DateTime(year, 1, 1);
+        }
+    }
+}
