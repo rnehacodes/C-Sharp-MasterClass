5a1d56ec46f729f52138bb0bca5d5239f7420d66
rnehacodes <neharajputcoding@gmail.com>
2024-05-23 23:53:12 +0530

    Garbage Collection

---
+++ GarbageCollector/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace GarbageCollector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            Console.WriteLine("Value of a before calling garbage collector method: "+ a);
+
+            GC.Collect();
+
+            Console.WriteLine("Value of a after calling garbage collector method: " + a);
+        }
+    }
+}
