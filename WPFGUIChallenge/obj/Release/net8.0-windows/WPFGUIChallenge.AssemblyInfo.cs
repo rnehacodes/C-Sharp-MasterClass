9575acc551361fcb50759a038c9a252fe4a5da88
rnehacodes <neharajputcoding@gmail.com>
2024-08-08 23:58:25 +0530

    File Update

--- WPFGUIChallenge/obj/Release/net8.0-windows/WPFGUIChallenge.AssemblyInfo.cs
+++ WPFGUIChallenge/obj/Release/net8.0-windows/WPFGUIChallenge.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WPFGUIChallenge")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e555d1c7d28448493e462b6efca424ee165973ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("WPFGUIChallenge")]
